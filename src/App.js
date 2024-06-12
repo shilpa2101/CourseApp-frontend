2c79bf85949d89592742688bf27e59ec98b6aed0
unknown <shilpas2101@gmail.com>
2024-06-12 18:26:29 +0530

    front end uploaded

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddCourse from './components/AddCourse';
+import View from './components/View';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<AddCourse/>}/>
+    <Route path='/view' element={<View/>}/>
+    <Route path='/search' element={<Search/>}/>
+    </Routes></BrowserRouter>
   );
 }
 
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Search = () => {
     const [datas,changedata]=useState(
@@ -6,24 +7,93 @@ const Search = () => {
             "title":""
         }
     )
+    const [result,setResult]=useState(
+        [
+            
+        ]
+    )
+
+    const deleteCourse=(id)=>{
+        let input={"_id" : id }
+        axios.post("http://localhost:8084/delete",input).then(
+            (response)=>{
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("succesfully deleted")
+                } else {
+                    alert("error")
+                }
+            }
+        ).catch()
+
+    }
+    // value fetching
      const inputHandler=(event)=>{
         changedata({...datas,[event.target.name]:event.target.value})
     }
+    // search button event
     const readValue=()=>{
         console.log(datas)
-    }
+        axios.post("http://localhost:8084/search",datas).then(
+            (response)=>{
+                setResult(response.data)
+            }
+
+        ).catch(
+            (error)=>{
+                console.log(error)
+            }
+        ).finally()
+
+    } 
   return (
     <div>
         <div className="container">
             <div className="row g-3">
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <label htmlFor="" className="form-label">Book title</label>
+                <label htmlFor="" className="form-label">course title</label>
                 <input type="text" className="form-control" name='title' value={datas.title} onChange={inputHandler}/>
             </div>
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                 <button className="btn btn-secondary" onClick={readValue}>Search</button>
             </div>
             </div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">title</th>
+                    <th scope="col">description</th>
+                    <th scope="col">date</th>
+                    <th scope="col">duration</th>
+                    <th>venue</th>
+                    <th>trainer name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {result.map(
+                        (value,index)=>{
+                            return <tr>
+                            <th >{value.title}</th>
+                            <td>{value.description}</td>
+                            <td>{value.date}</td>
+                            <td>{value.duration}</td>
+                            <td>{value.venue}</td>
+                            <td>{value.trainername}</td>
+                            <button className="btn btn-danger" onClick={()=>{deleteCourse(value._id)}}>Delete</button>
+                            </tr>
+                        }
+                    )}
+                    
+                    
+                </tbody>
+                </table>
+                </div>
+            </div>
+        </div>
+
         </div>
     </div>
   )
